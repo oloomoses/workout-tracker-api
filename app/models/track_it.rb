837f1cd40d727d2b96bf8fce8e80e73bd86dbcfe
oloomoses <olooine@gmail.com>
2021-06-06 18:39:19 +0300

    Update track_its_counter

--- app/models/track_it.rb
+++ app/models/track_it.rb
@@ -1,5 +1,5 @@
 class TrackIt < ApplicationRecord
-  belongs_to :workout
+  belongs_to :workout, counter_cache: true
   belongs_to :user
   validates_presence_of :calories, :minutes_recorded
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_205423) do
+ActiveRecord::Schema.define(version: 2021_06_06_143225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_05_25_205423) do
     t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "track_its_count", default: 0, null: false
   end
 
   add_foreign_key "track_its", "users"
