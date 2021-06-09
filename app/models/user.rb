ea7f410ea96c173bf84369bb9d4170c483015394
oloomoses <olooine@gmail.com>
2021-06-10 00:20:55 +0300

    Add test for valid email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :email, :password_digest
   validates_uniqueness_of :email
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
   it { should have_many(:track_its) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
+  it { should allow_value('user@example.com').for(:email) }
   it { should validate_presence_of(:password_digest) }
 end
